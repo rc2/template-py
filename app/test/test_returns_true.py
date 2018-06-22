436ea04bb858d4f9c0094cbf318d25df76f91ac2
rc2 <12886903+rc2@users.noreply.github.com>
2018-06-21 19:37:55 -0500

    updated test

--- app/test/test_returns_true.py
+++ app/test/test_returns_true.py
@@ -1,7 +1,7 @@
 import pytest
 
 # package import for test
-from package.sub_package.methods import returns_true
+from python_template.sub_package.methods import returns_true
 
 
 # test if returns_true returns true
