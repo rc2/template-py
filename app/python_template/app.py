1bd298a4fd59f9a78ff5eb9623251524e96e5c96
rc2 <12886903+rc2@users.noreply.github.com>
2018-06-21 20:24:53 -0500

    added version import, tied to docopt

--- app/python_template/app.py
+++ app/python_template/app.py
@@ -4,22 +4,29 @@ app
 
 usage:
   app -e ECHO [ -h -d ]
+  app [ -h -v ]
 
 arguments:
   -e,--echo=ECHO            echo text back to screen
 
 options:
-  -h,--help                 get help
+  -h,--help                 print help and exit
+  -v, --version             print version and exit
   -d,--debug                print debug info
 """
 from docopt import docopt
+import sys
+
+sys.path.append('.')
+from version import VERSION
 
 
 def main():
-  args = docopt(__doc__)
+  args = docopt(__doc__, version=VERSION)
   if args['--debug']:
     print(args)
-  print(args['--echo'])
+  if args['--echo'] and len(args['--echo']) > 0:
+    print(args['--echo'])
 
 
 if __name__ == "__main__":
